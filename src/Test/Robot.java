26bd333f162062654fa930ecba46d8a3df7338ea
DESKTOP-2KF083C\Garrett <garrettburroughs02@gmail.com>
2017-12-06 21:21:56 -0500

    Fixed Errors in Robot.java and FTCMotor.java

--- src/Test/Robot.java
+++ src/Test/Robot.java
@@ -9,4 +9,24 @@ import org.montclairrobotics.Alloy.Core.Alloy;
  */
 public class Robot extends Alloy{
 
+    /**
+     * The initialization method is were everything for the robot should be set up
+     * This includes driveTrain, motors, buttons, sensors etc.
+     * The initialization method will be run right after the core components are set up (RobotCore)
+     */
+    @Override
+    public void initilazation() {
+
+    }
+
+    /**
+     * Although most of the periodic actions are taken care by the updater, the user may want to
+     * add their own methods and code that need to be updated or run periodically, this can be done in
+     * the periodic() method
+     * periodic will be run every loop.
+     */
+    @Override
+    public void periodic() {
+
+    }
 }
--- src/org/montclairrobotics/Alloy/FTC/FTCMotor.java
+++ src/org/montclairrobotics/Alloy/FTC/FTCMotor.java
@@ -5,32 +5,62 @@ import org.montclairrobotics.Alloy.Core.Motor;
 /**
  * Created by Garrett on 11/14/2017.
  */
-public class FTCMotor implements Motor{
+public class FTCMotor implements Motor {
+    /**
+     * Sets the motor Power
+     *
+     * @param power the power that the motor will be set to (0-1 inclusive )
+     */
     @Override
-    public void setPower(int power) {
+    public void setPower(double power) {
 
     }
 
+    /**
+     * Gets the motor power
+     *
+     * @return the current motor power, a value between (0-1)
+     */
     @Override
     public double getPower() {
         return 0;
     }
 
+    /**
+     * Sets the motor position
+     *
+     * @param position the position the motor will be set to (in encoder ticks)
+     */
     @Override
     public void setPosition(int position) {
 
     }
 
+    /**
+     * Gets the motors position
+     *
+     * @return the position that the motor is at (in encoder ticks)
+     */
     @Override
     public double getPosition() {
         return 0;
     }
 
+    /**
+     * Sets weather the motor runs the default way , or inverted
+     *
+     * @param inverted true for inverted, false for normal
+     */
     @Override
     public void setInverted(boolean inverted) {
 
     }
 
+    /**
+     * Gets weather the motor is inverted
+     *
+     * @return true if the motor is inverted
+     */
     @Override
     public boolean getInverted() {
         return false;
